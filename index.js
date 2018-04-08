6faa33d1435fe280a4b057b4d31c096b8369418a
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-04-08 16:41:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+//function breakOut(array, changeValue, stopValue) {
+  //array.forEach( element => {
+    //array.splice(element[index],1,changeValue);
+//  }
+//};
+
 function breakOut(array, changeValue, stopValue) {
-  array.forEach( element => {
-    array.splice(element[index],1,changeValue);
-  }
-};
+  newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== stopValue) {
+      newArray[i] = changeValue;
+    } elseif (array[i] == stopValue) {
+      break
+    }
+ }
